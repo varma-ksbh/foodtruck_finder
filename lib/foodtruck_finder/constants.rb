10112509ca07a6cf0779b383f29a247cef677254
varma-ksbh <varma.ksbh7@gmail.com>
2019-10-24 00:46:48 -0400

    fixing gem install

--- lib/foodtruck_finder/constants.rb
+++ lib/foodtruck_finder/constants.rb
@@ -1,14 +1,16 @@
 require 'tmpdir'
 require 'logger'
 
-module FoodtruckFinder::Constants
-  SOCRATA_FOODTRUCK_URL = 'https://data.sfgov.org/resource/jjew-r69b.json'
+module FoodtruckFinder
+  module Constants
+    SOCRATA_FOODTRUCK_URL = 'https://data.sfgov.org/resource/jjew-r69b.json'
 
-  CONFIG_FILE = File.join(Dir.home, ".foodtruck-finder.config")
-  LOG_FILE = File.join(Dir.tmpdir(), "foodtruck-finder.log")
+    CONFIG_FILE = File.join(Dir.home, ".foodtruck-finder.config")
+    LOG_FILE = File.join(Dir.tmpdir(), "foodtruck-finder.log")
 
-  WELCOME_MESSAGE = "Feeling Hungry? Foodtruck Finder allows to find open restaurants near you! Enter `foodtruck_finder help`"
+    WELCOME_MESSAGE = "Feeling Hungry? Foodtruck Finder allows to find open restaurants near you! Enter `foodtruck_finder help`"
 
-  OPEN_FOODTRUCK_DISPLAY_KEYS = ['applicant', 'location']
-  OPEN_FOODTRUCK_DISPLAY_TITLE = 'Foodtrucks currently open in San Franciso'
+    OPEN_FOODTRUCK_DISPLAY_KEYS = ['applicant', 'location']
+    OPEN_FOODTRUCK_DISPLAY_TITLE = 'Foodtrucks currently open in San Franciso'
+  end
 end
